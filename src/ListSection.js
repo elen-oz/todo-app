872b9f5d61b3fe64ffaf3729e4e1ab62e3142690
Elena Ozerova <ozerova.lenka@gmail.com>
2022-10-13 13:21:31 +0200

    fix: error parent element

--- src/ListSection.js
+++ src/ListSection.js
@@ -12,33 +12,32 @@ function ListSection() {
   };
 
   return (
-    <div className='listSection'>
-      <h1>My TODO List</h1>
+    <div>
+      <div className='listSection'>
+        <h1>My TODO List</h1>
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type='text'
+          />
+          <button type='submit' onClick={addTodo}>
+            Add to do
+          </button>
+        </form>
 
-      <form>
-        <input
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          type='text'
-        />
-        <button type='submit' onClick={addTodo}>
-          Add to do
-        </button>
-      </form>
-
-      <div className='listSection_list'>
-        <h2 className='listSection_title'>List of Todos</h2>
-
-        <div className='listSection_elements'>
-          <ol className='listSection_ol'>
-            {todos.map((todo) => (
-              <li className='element' key='element'>
-                {todo}
-              </li>
-            ))}
-          </ol>
+        <div className='listSection_list'>
+          <h2 className='listSection_title'>List of Todos</h2>
+          <div className='listSection_elements'>
+            <ol className='listSection_ol'>
+              {todos.map((todo) => (
+                <li className='element' key='element'>
+                  {todo}
+                </li>
+              ))}
+            </ol>
+          </div>
         </div>
-        
       </div>
     </div>
   );
